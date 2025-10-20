51f7985108fd8cbbca91647887af1b66ba4d96c0
Abraão Lopes <abratreus.lopes@gmail.com>
2025-10-20 10:39:55 -0300

    Fix npm run deploy
    remove code of gpt :)

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom' // ou HashRouter
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './styles/base.css'
-import './styles/_variables.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
