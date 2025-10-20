d270514128520f2b66f25b58bed092fad7c1e7a0
Abraão Lopes <abratreus.lopes@gmail.com>
2025-10-20 16:23:29 -0300

    rm hero
    Remove hero

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import NavBar from '../components/NavBar/NavBar';
-import Hero from '../components/Hero/Hero';
-import Footer from '../components/Footer/Footer';
+import NavBar from '../Components/NavBar/NavBar';
+import Footer from '../Components/Footer/Footer';
 
 const Home = () => {
   return (
