34654bcb8685167837b7e21243fac834bb3b0138
Abraão Lopes <abratreus.lopes@gmail.com>
2025-10-20 11:00:37 -0300

    Fix links github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // ou HashRouter
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <HashRouter>
+    <App />
+  </HashRouter>
 )
