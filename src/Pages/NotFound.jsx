0c62ce59d881e26051ff5fec58653a6c015a9c3f
abratreus <abratreus.lopes@gmail.com>
2025-10-20 18:49:44 -0300

    Test

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import NavBar from '../Components/NavBar/NavBar';
-import Footer from '../Components/Footer/Footer';
+import NavBar from './Components/NavBar/NavBar';
+import Footer from './Components/Footer/Footer.jsx';
 
 const NotFound = () => {
   return (
