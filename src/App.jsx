7708c756759e13f8349bf9432aa8a89ef88a58a1
abratreus <abratreus.lopes@gmail.com>
2025-10-20 19:05:07 -0300

    n aguento mais

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './Components/NavBar/NavBar.jsx'
 import Footer from './Components/Footer/Footer.jsx'
-import Home from './pages/Home'
-import Catalogo from './pages/Catalogo'
-import Login from './pages/Login'
-import NotFound from './pages/NotFound'
-import Sobre from './pages/Sobre'
+import Home from './Pages/Home.jsx'
+import Catalogo from './Pages/Catalogo'
+import Login from './Pages/Login'
+import NotFound from './Pages/NotFound'
+import Sobre from './Pages/Sobre'
 
 function App() {
   return (
