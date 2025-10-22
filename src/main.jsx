81e2bf16d5fb03cda86d1b7d3cec387a28f7b180
Abraão Lopes <abratreus.lopes@gmail.com>
2025-10-22 09:13:10 -0300

    Fix the link of index.html
    C:\Users\Meu Computador\Documents\GitHub\WoodWork\dist\index.html

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Art-Moveis-WoodWork">
+    <BrowserRouter basename="/WoodWork">
       <App />
     </BrowserRouter>
   </React.StrictMode>
