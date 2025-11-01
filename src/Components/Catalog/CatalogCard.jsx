1b146a49205bd76e58aa2afe7bf808e8a5614adb
Abraão Lopes <abratreus.lopes@gmail.com>
2025-11-01 11:19:01 -0300

    Update CatalogCard.jsx

--- src/Components/Catalog/CatalogCard.jsx
+++ src/Components/Catalog/CatalogCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import viteSvg from '../../assets/react.svg';
+import logo from '../Logo/Logo.ico';
 import './CatalogCard.css';
 
 const CatalogCard = ({ title, description, image }) => {
   return (
     <div className="catalog-card">
-      <img src={viteSvg} alt={title} />
+      <img src={logo} alt={title} />
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
