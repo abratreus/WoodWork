87c4760c0a2880dd37f19fc640faa52371415a5c
Abraão Lopes <abratreus.lopes@gmail.com>
2025-11-01 16:57:03 -0300

    Update Login.jsx

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
+import loginImage from '../../assets/pexels-jeshoots-com-147458-834892.jpg';
 
 const Login = () => {
   const [credentials, setCredentials] = useState({ email: '', password: '' });
@@ -23,7 +24,7 @@ const Login = () => {
       <Row className="justify-content-center align-items-center min-vh-100">
         <Col md={6} className="d-none d-md-block">
           <div className="login-image">
-            <img src="/src/assets/pexels-jeshoots-com-147458-834892.jpg" alt="Login" className="img-fluid w-100 h-100 object-fit-cover" />
+            <img src={loginImage} alt="Login" className="img-fluid w-100 h-100 object-fit-cover" />
           </div>
         </Col>
         <Col md={6} className="d-flex flex-column justify-content-center align-items-center">
