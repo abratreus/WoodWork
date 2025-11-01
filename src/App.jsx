d8437afa687d594b41064a5e40f4821be47f3ff5
Abraão Lopes <abratreus.lopes@gmail.com>
2025-11-01 11:00:00 -0300

    update Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './Components/NavBar/NavBar.jsx'
 import Footer from './Components/Footer/Footer.jsx'
-import Home from './Pages/Home.jsx'
-import Catalogo from './Pages/Catalogo'
-import Login from './Pages/Login'
-import NotFound from './Pages/NotFound'
-import Sobre from './Pages/Sobre'
+import Home from './Pages/Home/Home.jsx'
+import Catalogo from ' ./Pages/Catalogo/Catalogo.jsx'
+import Login from './Pages/Login/Login.jsx'
+import NotFound from './Pages/NotFound/NotFound.jsx'
+import Sobre from './Pages/Sobre/Sobre.jsx'
 
 function App() {
   return (
