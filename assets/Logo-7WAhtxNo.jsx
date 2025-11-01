6cd5b3c739ecf752911162bd3c2288672d38da4b
abratreus <abratreus@users.noreply.github.com>
2025-11-01 14:11:29 +0000

    deploy: 36252c74ffef8584fc8df13f4ad7d8f488f25433

---
+++ assets/Logo-7WAhtxNo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from '../../assets/logo.ico';
+import './Logo.css';
+export default function componentName() {
+  return (
+    <>
+        <div className='Logo'></div>
+    </>
+  );
+}
